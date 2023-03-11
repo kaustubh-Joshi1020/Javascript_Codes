9e42f404ee268bca1fafc5a19091c432d05c4571
kaustubh-Joshi1020 <109413003+kaustubh-Joshi1020@users.noreply.github.com>
2023-03-11 22:48:27 +0530

    Add files via upload

---
+++ Playlist/Array_Praactice_Set.js
@@ -0,0 +1,30 @@
+// input number from user and add it to array
+//  let arr = [1,2,3,4,5]
+// let a = prompt("enter a number")
+// a = Number.parseInt(a)
+// arr.push(a)
+// console.log(arr)
+
+//filter number divisible by 10
+// let arr = [12,25,10,40,0,44,30]
+// let filturr = arr.filter((value)=>{
+//    return value%10==0;
+// })
+// console.log(filturr);
+
+//array of square of given number
+// let arr = [1,2,3,4,5,6,7,8]
+
+// let a = arr.map(element => {
+//     return element*element
+// });
+// console.log(a)
+
+
+//array of factorial of given number
+// let arr = [1,2,3,4,5]
+
+// let a = arr.reduce((element1,element2) => {
+//     return element1*element2;
+// });
+// console.log(a)
