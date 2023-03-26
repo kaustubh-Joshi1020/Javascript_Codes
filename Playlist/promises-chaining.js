87505306f4e991d84a5d6326e8acc642c5291432
kaustubh-Joshi1020 <109413003+kaustubh-Joshi1020@users.noreply.github.com>
2023-03-26 19:14:00 +0530

    Add files via upload

---
+++ Playlist/promises-chaining.js
@@ -0,0 +1,16 @@
+// let p1 = new Promise((resolve,reject)=>{
+//     resolve("this p1 promise is resolved")
+// })
+
+// p1.then((value)=>{
+//     console.log(value);
+//     console.log("this is first .then")
+// }).then(()=>{
+//     console.log("this is second .then")
+// }).then((value)=>{
+//     // return new Promise((resolve,reject)=>{
+//     //     console.log("this is last promise")
+//     // })
+// }).then(()=>{
+//     console.log("last msg")
+// })
